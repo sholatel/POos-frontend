91ada794fedec59c6e2c3f3d2cbf87c54f41f560
Yusuf <yusuforiade1616@gmail.com>
2024-12-27 09:31:36 +0100

    Update

--- src/app/config.js
+++ src/app/config.js
@@ -20,7 +20,7 @@ export const wagmiConfig = getDefaultConfig({
 
 
 export const API_URL = {
-    API_URL: process.env.NODE_ENV === "development" ? "http://localhost:5000/api/" : "https://y2sm8pjvyv.us-east-1.awsapprunner.com/api/",
+    API_URL: process.env.NODE_ENV === "development" ? "http://localhost:5000/api/" : process.env.NEXT_PUBLIC_FE_URL + "/api/",
     //PROD_URL: "https://y2sm8pjvyv.us-east-1.awsapprunner.com/api/",
 }
 
